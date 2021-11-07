47266cf430c2de88dc9a8cf549492183e984f1a1
Celeo <mattboulanger@fastmail.com>
2021-11-07 13:10:56 -0800

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,15 @@
 //!
 //! For the full Mattermost API information, see [their docs].
 //!
+//! To start, create an instance of the [`AuthenticationData`]
+//! struct, passing in either a login_id and password (likely
+//! email and password), or a personal access token. Pass this
+//! struct instance along with the URL of the target Mattermost
+//! instance to [`Mattermost::new`].
+//!
 //! [their docs]: https://api.mattermost.com
+//! [`AuthenticationData`]: struct.AuthenticationData.html
+//! [`Mattermost::new`]: struct.Mattermost.html
 
 #![deny(clippy::all)]
 #![deny(unsafe_code)]
